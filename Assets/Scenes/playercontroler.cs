7d889ff1430a8d12140866a9e2f5dafed6b4e0cd
bwaobikeze <bwaobikeze@hotmail.com>
2020-08-23 23:14:25 -0500

    added jump

--- Assets/Scenes/playercontroler.cs
+++ Assets/Scenes/playercontroler.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class playercontroler : MonoBehaviour
 {
     // Start is called before the first frame update
+    bool grounded= false;
+    public Transform groundCheck;
+    public LayerMask groundlayer;
+    float groundCheckRadius = 0.2f;
+    public float jumpHeight;
     Animator myAnim;
     Rigidbody2D body;
     bool frontfacingDirection;
@@ -20,6 +25,14 @@ public class playercontroler : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
+
+      // To check if grounded
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundlayer);
+        myAnim.SetBool("isGrounded", grounded);
+        myAnim.SetFloat("verticalSpeed", body.velocity.y);
+
+        //player movement
           float move = Input.GetAxis("Horizontal");
         myAnim.SetFloat("speed", Mathf.Abs(move));
 
@@ -33,6 +46,16 @@ public class playercontroler : MonoBehaviour
         else if (move < 0 && frontfacingDirection)
             flip();
 
+    }
+     void Update() {
+       // jumping
+      if (grounded && Input.GetAxis("Jump") > 0)
+        {
+            grounded = false;
+            myAnim.SetBool("isGrounded", grounded);
+            body.AddForce(new Vector2(0, jumpHeight));
+
+        }
     }
 
 
